a9ea84307bc3f949e9481b5c589cf464b4a538e7
Йоана Матева <107748308+YoanaSM@users.noreply.github.com>
2023-07-29 12:12:28 +0300

    Update 4th Problem Intersection.cpp

--- 4. Arrays/4th Problem Intersection.cpp
+++ 4. Arrays/4th Problem Intersection.cpp
@@ -9,7 +9,7 @@ int main()
    int n, m;
    std :: cin >> n >> m;
 
-
+//input of th elements
     for (int i = 0; i < n ; ++i)
     {
         std::cin >> arr1[i];
@@ -20,32 +20,32 @@ int main()
         std::cin >> arr2[j];
     }
 
-    int intersection[MAX_SIZE];
-    int count = 0;
+    int intersection[MAX_SIZE];// array for the intersection of arr1 and arr2
+    int count = 0;// counting the same elements
 
     for (int i = 0; i < n ; i++)
     {
-        bool isArr2 = false;
+        bool isArr2 = false; // checking if any element in arr1 is the same as one in arr2
 
         for (int j = 0; j < m; j++)
         {
             if (arr1[i] == arr2[j])
             {
-                isArr2 = true;
+                isArr2 = true; //if they are the same the bool is switched to true
                 break;
             }
         }
-        if (isArr2)
+        if (isArr2)// when we have a match we put the element in the third array
         {
             intersection[count] = arr1[i];
-            count++;
+            count++;//increase the count variable
         }
 
     }
-    std :: cout << count << std :: endl;
+    std :: cout << count << std :: endl;//print how many elements are the same
 
     for (int i = 0; i < count; i++)
-        std :: cout << intersection[i] << " ";
+        std :: cout << intersection[i] << " "; print the third array
 
     std :: cout << std :: endl;
 
